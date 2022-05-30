b32053ecd5ad6beb3addf47ae80020511ca8a436
Ahmet Akcan <64594414+ahmettakcan@users.noreply.github.com>
2022-05-30 11:44:21 +0300

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,32 +5,32 @@ list = ["Rock","Paper","Scissors"]
 user_val = 0
 machine_val = 0
 while True:
-  user = input("Rock, Paper, Scissors ?\n")
-  machine = choice(list)
-  print(machine)
+    user = input("Rock, Paper, Scissors ?\n")
+    machine = choice(list)
+    print(machine)
   
-  if (user == list[0] and machine == list[2]) or (user == list[2] and machine == list[1]) or (user == list[1] and machine == list[0]):
-    print("User won the round")
-    user_val+=1
+    if (user == list[0] and machine == list[2]) or (user == list[2] and machine == list[1]) or (user == list[1] and machine == list[0]):
+        print("User won the round")
+        user_val+=1
   
-  elif (machine == list[0] and user == list[2]) or (machine == list[2] and user == list[1]) or (machine == list[1] and user == list[0]):
-    print("Machine won the round")
-    machine_val+=1
+    elif (machine == list[0] and user == list[2]) or (machine == list[2] and user == list[1]) or (machine == list[1] and user == list[0]):
+        print("Machine won the round")
+        machine_val+=1
   
-  else:
-    print("The round is draw")
+    else:
+        print("The round is draw")
 
-  print(f"User's : {user_val} | Machine's : {machine_val}")
-  total = input("Do you wanna play again ?\n")
+    print(f"User's : {user_val} | Machine's : {machine_val}")
+    total = input("Do you wanna play again ?\n")
 
-  if total != "yes":
-    break
+    if total != "yes":
+        break
 
 if user_val > machine_val:
-  print("User Won")
+    print("User Won")
 
 elif machine_val > user_val:
-  print("Machine Won")
+    print("Machine Won")
 
 else:
-  print("Draw")
+    print("Draw")
